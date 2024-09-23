bef941df46357ee6dab4674c72085738b26a99e5
CarlosEduardoLemosNascimento <159453764+CarlosEduardoLemosNascimento@users.noreply.github.com>
2024-09-23 11:59:49 +0000

    .

--- projeto/tests/test_pessoa.py
+++ projeto/tests/test_pessoa.py
@@ -11,12 +11,12 @@ def test_pessoa_alterar_nome_valido(pessoa_valida):
     pessoa_valida.nome = "Vitoria"
     assert pessoa_valida.nome == "Vitoria"
 
-#def test_pessoa_nome_valido(pessoa_valida):
-#    assert pessoa_valida.nome == "Marta"
+def test_pessoa_nome_valido(pessoa_valida):
+    assert pessoa_valida.nome == "Marta"
 
-#def test_pessoa_idade_valida(pessoa_valida):
-#    assert pessoa_valida.idade == 23
+def test_pessoa_idade_valida(pessoa_valida):
+    assert pessoa_valida.idade == 23
 
-#def test_pessoa_idade_negativa_retorna_mensagem_excecao(pessoa_valida):
-#    with pytest.raises(ValueError, match="Idade não pode ser negativa."):
-#        Pessoa("Marta", -1)
+def test_pessoa_idade_negativa_retorna_mensagem_excecao(pessoa_valida):
+    with pytest.raises(ValueError, match="Idade não pode ser negativa."):
+        Pessoa("Marta", -1)
